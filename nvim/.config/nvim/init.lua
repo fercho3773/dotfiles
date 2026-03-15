157c69b067900123d2fe53b2bb91e58c17a9ee3e
Fernando <fdnh.navarro@gmail.com>
2026-03-15 14:31:08 -0600

    Tweaked neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,11 @@
 -- Thanks to Radley E. Sidwell-lewis, the original author for the suckless neovim config
 
+require("plug")  -- plugins
 require("func")  -- functions
 require("keys")  -- keybinds
 require("opts")  -- options
 require("stln")  -- statusline
 require("tabs")  -- tabs
 require("term")  -- terminal
+
+vim.pack.add(require("plugins.luasnip"))
