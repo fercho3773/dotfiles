34e4393e9e36fe53786351ed206a7e51ef8a5785
Fernando David <fdnh.navarro@gmail.com>
2026-07-10 01:18:50 -0600

    Nvim snippets

---
+++ nvim/snippets/tex.lua
@@ -0,0 +1,27 @@
+return {
+  --
+  require("luasnip").snippet(
+    { trig = "alp" },
+    { t("\alpha") }
+  ),
+  --
+  require("luasnip").snippet(
+    { trig = "bet" },
+    { t("\beta") }
+  ),
+  --
+  require("luasnip").snippet(
+    { trig = "gam" },
+    { t("\gamma") }
+  ),
+  --
+  require("luasnip").snippet(
+    { trig = "delt" },
+    { t("\delta") }
+  ),
+  --
+  require("luasnip").snippet(
+    { trig = "eps" },
+    { t("\epsilon") }
+  ),
+}
