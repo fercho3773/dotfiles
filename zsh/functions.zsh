5e4333df0b03e6f08c6f2f350dd8cd287a6e83c8
Fernando David <fdnh.navarro@gmail.com>
2026-07-28 13:17:29 -0600

    Now fzf has its own .zsh file

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -62,23 +62,6 @@ case "$1" in
 esac
 }
 
-# fzf
-fzfcleanfind() {
-  clear | fd --type f --exclude .git --exclude dist | fzf
-}
-zle -N fzfcleanfind
-bindkey '^F' fzfcleanfind
-
-# fzf fuzzy finder
-_fzf_file_no_hidden() {
-  local cmd result
-  cmd="${FZF_DEFAULT_COMMAND/--hidden /}"
-  result=$(eval "${cmd:-find . -type f}" | fzf --preview "$_FZF_PREVIEW_CMD") \
-    && LBUFFER+="$result"  # LBUFFER is the text left of the cursor
-  zle reset-prompt
-}
-zle -N _fzf_file_no_hidden
-
 # find man pages
 #fman() {
 #    man -k . | 
